54fbf182ca0dd09070627d661d93b25c936faff3
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:40:43 +0530

    Add files via upload

---
+++ Looping statements.py
@@ -0,0 +1,29 @@
+# while loop
+#while(condition1):
+   #statement1
+   #increment/decrement operation
+#rest of the program
+
+# Nested while Loop (while loop within while loop)
+# i=1
+# j=5
+# while (i<4):
+#     while (j<8):
+#         print(i,j)
+#         i=i+1
+#         j=j+1
+
+# While loop with else statement
+# a=1
+# while (a<=5):
+#     print(a)
+#     a=a+1
+# else:
+#     print("I am from else block")
+
+#infinite while loop
+#(condition1):
+#statement1
+
+# For loop
+ 
