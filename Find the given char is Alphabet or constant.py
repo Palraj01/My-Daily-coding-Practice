5e5017ca5f86f320328b6efa3baa46953f94feb7
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:38:51 +0530

    Add files via upload

---
+++ Find the given char is Alphabet or constant.py
@@ -0,0 +1,16 @@
+#Find the given char is Alphabet or constant
+# ch=input("Enter the character:\n")
+#
+# if (ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z'):
+#     print(ch, "is Alphabet")
+# else:
+#     print(ch, "is Constant")
+
+#Find the given char is Vowel or consonant
+
+ch=input("Enter the character:\n")
+
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u') or (ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print(ch, "is Vowel")
+else:
+    print(ch, "is Consonant")
