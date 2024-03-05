697d529b104f72bc4faf672c8dc924e37a68ae7f
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:44:48 +0530

    Add files via upload

---
+++ Use of Pass statement.py
@@ -0,0 +1,15 @@
+
+# i=1
+# while i<=10:
+#    if(i==5):
+#         pass
+#    print("pass statement found")
+#    print(i)
+# print("program terminated")
+
+for i in range(10):
+    if i==5:
+        pass
+        print("the pass statement is there")
+    else:
+        print(i) 
