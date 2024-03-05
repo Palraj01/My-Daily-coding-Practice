736fca2952025f557ecdf0156acb22ed82d64246
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:44:23 +0530

    Add files via upload

---
+++ Use of Break statement.py
@@ -0,0 +1,17 @@
+# Using in while statement
+# a=0
+# while a<=5:
+#     if (a==3):
+#          break
+#     a=a+1
+#     print(a)
+# print("leave from the loop")
+
+# Using in for statement
+
+for le in "happy learning":
+    if le =='y':
+        break
+    else:
+        print(le)
+print("for loop terminated")
