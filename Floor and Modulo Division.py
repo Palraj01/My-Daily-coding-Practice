4b43651397ffb333f6cdae8e04e470dbb5239935
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:40:19 +0530

    Add files via upload

---
+++ Floor and Modulo Division.py
@@ -0,0 +1,27 @@
+# By using normal division ,(NOTE:it will give exact value of quotient ,
+# but also it provides the reminder of 1)(This will be corrected by using Floor Division)
+# a=int(input("Enter a Number:"))
+# b=int(input("Enter a number:"))
+# quotient=a/b
+# reminder=a%b
+# print("quotient of",a,"is:",quotient)
+# print("reminder of",a,"is:",reminder)
+
+#By Using Floor Division (eg :13/2==> q=6,r=1)
+# (but here we cant use float , it will give value error)
+
+# a=int(input("Enter a Number:"))
+# b=int(input("Enter a number:"))
+# quotient=a//b
+# reminder=a%b
+# print("quotient of",a,"is:",quotient)
+# print("reminder of",a,"is:",reminder)
+
+#By using Float in the Floor Division
+
+a=float(input("Enter a Number:"))
+b=float(input("Enter a number:"))
+quotient=a//b
+reminder=a%b
+print("quotient of",a,"is:",quotient)
+print("reminder of",a,"is:",reminder)
