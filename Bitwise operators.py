b850e19da71dafe19a8712877537acfce213db38
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:35:29 +0530

    Add files via upload

---
+++ Bitwise operators.py
@@ -0,0 +1,31 @@
+# Bitwise NOT Operator(also called Unary Operator,Operates with one operand) (a=-a-1) (7=-7-1=-8)
+
+# a=7
+# print(~a)
+
+# Bitwise XOR Operator  (XOR Table : 0 0 :0 , 0 1 :1 , 1 0 :1 , 1 1 : 0)
+# The O/P will be True(1),if one of the I/P is False
+# a=20
+# b=4
+# print(a^b)
+
+# Bitwise OR operator (0 0:0, 0 1:1 , 1 0:1 , 1 1:1)
+
+# a=10
+# b=4
+# print(a|b)
+
+# Bitwise AND Operator (0 0:0 , 0 1:0 , 1 0:1 , 1 1:1)
+
+# a=12
+# b=13
+# print(a & b)
+
+# Bitwise Left shift operator (the left operand value is moved left by the number of operands present in the Right operand)
+
+# a=20
+# print(a<<2)
+
+#Bitwise Right shift operator (the right operand value is moved right by the number of operands present in the Right operand)
+a=20
+print(a>>2)
