a52be5ce4a2e3c0bdfe6419b57caaf9f93a76e37
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:38:31 +0530

    Add files via upload

---
+++ Find the avg of given numbers.py
@@ -0,0 +1,17 @@
+
+n=int(input("Enter how many numbers:"))
+a=[]
+
+for i in range(0,n):
+    element=int(input("Enter the number:"))
+    a.append(element)
+avg=sum(a)/n
+
+print(avg)
+
+
+
+
+
+
+
