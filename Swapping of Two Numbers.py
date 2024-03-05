bf43a9259b8e9a858886c1ae686d7a4cf6a36dba
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:43:44 +0530

    Add files via upload

---
+++ Swapping of Two Numbers.py
@@ -0,0 +1,16 @@
+#1st Method , By Assigning Temp Value
+# a="10"
+# b="20"
+# print("before Swapping:" ,a,b)
+# c=a
+# a=b
+# b=c
+# print("after swapping:" , a,b)
+
+#By Swap function , By Assigning Swap function
+
+a="10"
+b="20"
+print("before Swapping:" ,a,b)
+a,b=b,a
+print("after swapping:" , a,b)
