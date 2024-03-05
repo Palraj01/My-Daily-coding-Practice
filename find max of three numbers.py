0e2c96cae8bf39291d3d4a68ef033df042fa4bc6
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:37:10 +0530

    Add files via upload

---
+++ find max of three numbers.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+
+if (a>=b and a>=c):
+    print("max is a:",a)
+elif (b>=a and b>=4c):
+    print("max is b:",b)
+else:
+    print("max is c:",c)
