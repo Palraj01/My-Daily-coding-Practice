9beadf37749409a7ee92160da72e7dba2add29a1
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:44:35 +0530

    Add files via upload

---
+++ Use of continue statement.py
@@ -0,0 +1,16 @@
+#
+# for value in "happy learning":
+#     if value=="y":
+#         continue
+#     else:
+#         print(value)
+# print("program Terminated")
+
+#
+for i in range (1,10):
+    if(i%2==0):
+        continue
+    print(i)
+print("end")
+
+
