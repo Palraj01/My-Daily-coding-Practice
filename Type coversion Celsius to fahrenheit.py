676cbde8299c8d67c63a47c29f006f42c16361e3
Palraj01 <147310538+Palraj01@users.noreply.github.com>
2024-03-05 19:44:09 +0530

    Add files via upload

---
+++ Type coversion Celsius to fahrenheit.py
@@ -0,0 +1,14 @@
+#converting celsius to fahrenheit
+
+# celsius=int(input("Enter the value:"))
+# fahrenheit=(celsius*1.8)+32
+# print("%0.2f celsius is equals to %0.2f fahrenheit" %(celsius,fahrenheit))
+
+#converting fahrenheit to celsius
+
+fahrenheit=int(input("Enter the value:"))
+celsius=(fahrenheit-32)/1.8
+print("%0.2f fahrenheit is equals to %0.2f celsius" %(fahrenheit,celsius))
+
+
+
